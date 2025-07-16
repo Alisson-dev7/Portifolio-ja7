16903ccd7fbb9c30e36789edbe625f63df25cc58
Alisson <josealisson977.dev@gmail.com>
2025-07-16 08:52:00 -0300

    concertando

--- src/pages/Projetos/Projetos.tsx
+++ src/pages/Projetos/Projetos.tsx
@@ -1,7 +1,5 @@
 import { BoxProjetos } from '../../components/BoxProjetos';
 import styles from './styles.module.css';
-import vangoghImg from '../../assets/img-projetos/vangogh.png';
-import recantoPeixeImg from '../../assets/img-projetos/RecantoPeixe.png';
 
 export function Projetos() {
     return (
@@ -10,8 +8,8 @@ export function Projetos() {
                 <h1>Projetos</h1>
             </div>
             <div className={styles.projetos_content}>
-                <BoxProjetos img={vangoghImg} title="Vangogh" description="Site dedicado ao pintor Vicent Van Gogh, nesse site mostra as suas famosas obras, frases, poemas com referência a ele e as suas pinturas, e um cátalago de produtos para quem é fã." link="https://vangogh7.netlify.app" />
-                <BoxProjetos img={recantoPeixeImg} title="Recanto do Peixe" description="Site de restaurante especializado em peixes, com cardápio, informações de contato e localização." link="https://recanto-do-peixe.netlify.app"/>
+                <BoxProjetos img="/portifolio_/src/assets/vangogh.png" title="Vangogh" description="Site dedicado ao pintor Vicent Van Gogh, nesse site mostra as suas famosas obras, frases, poemas com referência a ele e as suas pinturas, e um cátalago de produtos para quem é fã." link="https://vangogh7.netlify.app" />
+                <BoxProjetos img="/portifolio_/src/assets/RecantoPeixe.png" title="Recanto do Peixe" description="Site de restaurante especializado em peixes, com cardápio, informações de contato e localização." link="https://recanto-do-peixe.netlify.app"/>
             </div>
         </section>
     )
